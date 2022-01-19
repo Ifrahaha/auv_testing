f49dc33064454aae8ad399227cd5360e82d9c860
Zahid Hussain <zahidhussain909@gmail.com>
2022-01-19 15:00:05 +0530

    variable typos

--- AUV2k19/motion/obstacle_det.py
+++ AUV2k19/motion/obstacle_det.py
@@ -10,25 +10,25 @@ def move_angle(angle):
 	if angle < 0: 
 		angle = angle + 360 
 	bearing = readDirection() 
-	move angle = bearing - angle 
+	move_angle = bearing - angle 
 	if move_angle > 180: 
 		turn_right() 
 	elif move_angle < -180: 
 		turn_left() 
 	elif (move_angle) < 180 and move_angle > 0: 
-		turn left() 
+		turn_left() 
 	elif move_angle > -180 and move_angle < 0: 
 		turn_right() 
 	while(abs(angle - bearing)) > 5: 
 		time.sleep(.2) 
-		print abs(angle-bearing) 
+		print(abs(angle-bearing))
 		bearing = readDirection() 
 	stop() 
 	print("angle", bearing)
 	
 	
 def positionRobot(xpos, ypos, xpos_goal, ypos_goal): 
-	print xpos, ypos, xpos_goal, ypos_goal 
+	print (xpos, ypos, xpos_goal, ypos_goal)
 	distance = math.sqrt((xpos_goal - ypos_robot)**2 + (ypos_goal - ypos_robot)**2) 
 	angle = round(math.degrees(math.atan2((ypos_goal - ypos_robot), xpos_goal - xpos_robot)))) 
 	print("angle",angle)
@@ -43,7 +43,7 @@ ypos_goal = int(raw_input("Goal Y Position: "))
 
 distance, angle = positionRobot(xpos_robot, ypos_robot, xpos_goal, ypos_goal) 
 
-start time = time.time() 
+start_time = time.time() 
 forward() 
 barrier = rr.get_distance() 
 elapsed_time = 0 
@@ -52,7 +52,7 @@ while barrier > 10 and elapsed_time < distance:
 	barrier = rr.get_distance() 
 	if barrier > 0 and barrier < 10: 
 		print("barrier", barrier) 
-		distance_traveled = elapsed time 
+		distance_traveled = elapsed_time 
 		new_distance = 1 
 		ypos_robot = ypos_robot + distance_traveled * math.sin(math.radians(angle)) 
 		ypos_goal_barrier = ypos_robot + new_distance * math.sin(math.radians(angle + 90)) 
@@ -62,13 +62,13 @@ while barrier > 10 and elapsed_time < distance:
 		start_time = time.time() 
 		forward() 
 		elapsed_time = 0 
-		while elapsed_time < new distance: 
+		while elapsed_time < new_distance: 
 			elapsed_time = time.time() - start_time 
-		print "Done moving around barrier" 
-		ypos_robot = ypos_goal barrier 
-		xpos_robot = xpos_goal barrier 
+		print("Done moving around barrier")
+		ypos_robot = ypos_goal_barrier 
+		xpos_robot = xpos_goal_barrier 
 		distance = positionRobot(xpos_robot, ypos_robot, xpos_goal, ypos_goal) 
-		start time = time.time() 
+		start_time = time.time() 
 		forward() 
 		barrier = rr.get_distance() 
 		elapsed_time = 0 
